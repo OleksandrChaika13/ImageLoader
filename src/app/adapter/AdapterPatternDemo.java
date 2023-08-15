8a181cbaf5037addfd9a8f53d05a3881c04ee9b2
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-15 11:04:20 +0300

    Creating "AdapterPatternDemo" class

--- src/app/adapter/AdapterPatternDemo.java
+++ src/app/adapter/AdapterPatternDemo.java
@@ -1,4 +1,15 @@
 package app.adapter;
 
+import app.loader.ImageLoader;
+
 public class AdapterPatternDemo {
+
+    public static void adapter(){
+        ImageLoader imageLoader = new ImageLoader();
+
+        imageLoader.launch("heic", "vacation2021.heic");
+        imageLoader.launch("jpg", "birthday0404.jpg");
+        imageLoader.launch("png", "selfie.png");
+        imageLoader.launch("gif", "meme.gif");
+    }
 }
