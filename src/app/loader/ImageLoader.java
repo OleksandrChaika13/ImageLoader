83d03d121e8454612742f42698edbd6a8461527b
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-15 11:03:20 +0300

    Creating "ImageLoader" class

--- src/app/loader/ImageLoader.java
+++ src/app/loader/ImageLoader.java
@@ -1,4 +1,26 @@
 package app.loader;
 
-public class ImageLoader {
+import app.adapter.MediaAdapter;
+
+public class ImageLoader implements IMediaLoader {
+
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void launch(String pictureExtension, String pictureName) {
+
+        if (pictureExtension.equalsIgnoreCase("heic")) {
+            System.out.println("Opening heic image: " + pictureName);
+        }
+
+        else if (pictureExtension.equalsIgnoreCase("jpg") ||
+                pictureExtension.equalsIgnoreCase("png")
+        ) {
+            mediaAdapter = new MediaAdapter(pictureExtension);
+            mediaAdapter.launch(pictureExtension, pictureName);
+        } else {
+            System.out.println("Invalid file format: " + pictureExtension
+                    + " is not supported :(");
+        }
+    }
 }
