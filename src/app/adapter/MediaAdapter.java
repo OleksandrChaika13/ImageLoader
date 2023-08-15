5e5a661b53c7a87875be1b23f2118d5b5f455aae
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-15 11:02:27 +0300

    Creating "MediaAdapter" class

--- src/app/adapter/MediaAdapter.java
+++ src/app/adapter/MediaAdapter.java
@@ -1,4 +1,30 @@
 package app.adapter;
 
-public class MediaAdapter {
+import app.loader.IAdvancedLoader;
+import app.loader.IMediaLoader;
+import app.player.JpgPlayer;
+import app.player.PngPlayer;
+
+public class MediaAdapter implements IMediaLoader {
+
+    IAdvancedLoader advancedLoader;
+
+    public MediaAdapter(String pictureExtension) {
+
+
+        if (pictureExtension.equalsIgnoreCase("jpg")) {
+            advancedLoader = new JpgPlayer();
+        } else if (pictureExtension.equalsIgnoreCase("png")) {
+            advancedLoader = new PngPlayer();
+        }
+    }
+
+    @Override
+    public void launch(String pictureExtension, String pictureName) {
+        if (pictureExtension.equalsIgnoreCase("jpg")) {
+            advancedLoader.openJpg(pictureName);
+        } else if (pictureExtension.equalsIgnoreCase("png")) {
+            advancedLoader.openPng(pictureName);
+        }
+    }
 }
